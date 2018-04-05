aed8d7e929525963c43229a23b0b1328d067c7dc
DanMcCabeSD <danieljm@udel.edu>
2018-04-04 19:31:26 -0700

    final fix

--- liri.js
+++ liri.js
@@ -7,57 +7,92 @@ require("dotenv").config();
  var Spotify = require('node-spotify-api');
  var request = require('request');
  var fs = require('fs');
-
-  var client = new Twitter(keys.twitter);
+ var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 var getMyTweets = function() {   
 
-var client = new Twitter(keys.twitter);
- 
-var params = {screen_name:"UCSDhomework"};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (i = 0; i < tweets.length; i++) { 
-    console.log(tweets[i].text);
-}
+  var client = new Twitter(keys.twitter);
    
+  var params = {screen_name:"UCSDhomework"};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for (i = 0; i < tweets.length; i++) { 
+      console.log(tweets[i].text);
+    }  
   }
 });
 
 //do not have spotify account attached
 	}
-var getMeSpotify = function() {   
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+var getMeSpotify = function(songName) {   
+  spotify.search(
+  { 
+    type: 'track',
+    query: songName
+  }, 
+  function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
+    var songs= data.tracks.items;
+    for (var i = 0; i < songs.length; i++) {
+        console.log (i);
+        console.log (songs[i].album.name);
+    }
+  //  console.log(data.tracks.items); 
   }
- 
-console.log(data); 
-});
-	}
+  );
+	};
 
-var getMeMovie = function() {   
-	
-	}
+var getMeMovie = function(movieName) {   
+
+  var APIKey = "trilogy";
+	 var queryURL =  "http://www.omdbapi.com/?t=" + movieName + "&apikey=" + APIKey;
+
+request(queryURL, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+var jsonData= JSON.parse(body);
+  console.log (jsonData.Title);
+  console.log (jsonData.Year);
+
+});
+	};
 var doWhatItSays = function() {   
-	
-	}
+	fs.readFile ("random.txt", "utf8", function (err, data){
+//    console.log (data);
+  var dataArray = data.split(",");
+  if (dataArray.length===2){
+    pick(dataArray[0], dataArray[1]);
+  }
+  else if (dataArray.length===1){
+    pick(dataArray[0]);
+  }
+  });
+	};
 // Function for determining which command is executed 
 var pick = function(caseData, functionData) {  
  switch (caseData) { 
 
     case "my-tweets":       
     getMyTweets();       
-    break;     
+    break; 
+    
     case "spotify-this-song":       
     getMeSpotify(functionData);       
-    break;     
+    break;   
+
     case "movie-this":       
     getMeMovie(functionData);       
-    break;     
+    break;    
+
     case "do-what-it-says":       
     doWhatItSays();       
-    break;     
+    break;    
+
     default:  
     console.log("LIRI doesn't know that");   
 	} 
